de411d9a6bbaecffe92661a5d9dc9b160e8c893d
Nathan C <52353385+rbwk@users.noreply.github.com>
2023-12-21 15:04:29 -0500

    added mainmenu

---
+++ src/mainMenu.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class mainMenu {
+
+	static JFrame f = new JFrame("Exciting Guess Who Game!!!"); //JFrame for the whole menu
+	static JButton exitButton = new JButton(); //Button for exiting the program
+	
+	
+
+	
+	
+	public static void main(String[] args) throws InterruptedException {
+		setFrame();	
+	}
+	
+	public static void setFrame() {	
+		f.setSize(1000,600);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+
+		exitButton = new JButton("Exit");
+		exitButton.setBounds(0, 330, 100, 30);
+		exitButton.addActionListener(new exitButton());
+		f.add(exitButton);
+		
+
+		
+		f.setLayout(new SpringLayout());
+		f.setVisible(true);
+	}
+	
+	
+	public static void componentResized() { //Make Better lock ratio method
+	    int W = 5;  
+	    int H = 3;  
+	    Rectangle b = f.getBounds();
+	    f.setBounds(b.x, b.y, b.width, b.width*H/W);
+
+	}
+	
+	static class buttonClick implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			JButton clickedButton = (JButton) e.getSource();
+			
+		}	}
+}
