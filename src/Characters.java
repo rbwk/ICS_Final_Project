20b931a9f501029a2606ab67f280d78429063a4c
victoriaachi <victoria.chi26@gmail.com>
2023-12-21 14:08:27 -0500

    Update Characters.java

--- src/Characters.java
+++ src/Characters.java
@@ -1,36 +1,60 @@
 public class Characters {
-    
-    //Characteristics (Maybe a menu for customizing characters)
+
+    // Characteristics (Maybe a menu for customizing characters)
     protected String name;
     protected String sex;
-    protected String hair_length;
+    protected String eye_colour;
     protected String skin_tone;
+    protected String hair_colour;
+    protected boolean facial_hair;
+    protected boolean glasses;
+    protected boolean showing_teeth;
+    protected boolean wearing_hat;
+    protected String hair_length;
+    protected boolean piercings;
 
-    //Make more indepth in seperate program. 
-    protected boolean accessory;
-    
+    // Make more indepth in seperate program.
 
-    //Constructor Methods
-    public Characters(){
+    // Constructor Methods
+    public Characters() {
         name = null;
         sex = null;
+        eye_colour = null;
+        skin_tone = null;
+        hair_colour = null;
+        facial_hair = null;
+        glasses = null;
+        showing_teeth = null;
+        wearing_hat = null;
         hair_length = null;
+        piercings = null;
     }
 
-    //Behaviours
-    public void setName(String newName){
+    // Behaviours
+    public void setName(String newName) {
         name = newName;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
 
-    public void setSex(String newSex){
+    public void setSex(String newSex) {
         sex = newSex;
     }
-    public String getSex(){
+
+    public String getSex() {
         return sex;
     }
 
+    public void setEyeColour(String newEyeColour) {
+        eye_colour = newEyeColour;
+    }
+
+    public String getEyeColour() {
+        return eye_colour;
+    }
+
+    public void set
 
 }
