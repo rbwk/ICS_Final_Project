1054ce67bcc1199cfddb5a6e860dd96615be6ef2
Nathan C <iamnathanchu@gmail.com>
2023-12-23 21:13:14 -0500

    changes to mainmenu and some extra things

--- src/mainMenu.java
+++ src/mainMenu.java
@@ -6,11 +6,15 @@ import javax.swing.*;
 
 
 public class mainMenu {
-
+	
+	static JPanel buttonPanel = new JPanel(new FlowLayout(1,1000,15));
+	static JPanel blankPanel = new JPanel();
+	
 	static JFrame f = new JFrame("Exciting Guess Who Game!!!"); //JFrame for the whole menu
+	static JButton startButton = new JButton(); //Button for starting the game
+	static JButton leaderboardButton = new JButton(); //Button for leaderboard
+	static JButton creditsButton = new JButton(); //Button for credits
 	static JButton exitButton = new JButton(); //Button for exiting the program
-	
-	
 
 	
 	
@@ -21,16 +25,29 @@ public class mainMenu {
 	public static void setFrame() {	
 		f.setSize(1000,600);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setLayout(new GridLayout(3,3));
 		
-
+		startButton = new JButton("Start Game!");
+		startButton.addActionListener(new startButton());
+		buttonPanel.add(startButton);
+		
+		leaderboardButton = new JButton("Leaderboard");
+		leaderboardButton.addActionListener(new leaderboardButton());
+		buttonPanel.add(leaderboardButton);
+		
+		creditsButton = new JButton("Credits");
+		creditsButton.addActionListener(new creditsButton());
+		buttonPanel.add(creditsButton);
+		
 		exitButton = new JButton("Exit");
-		exitButton.setBounds(0, 330, 100, 30);
-		exitButton.addActionListener(new buttonClick());
-		f.add(exitButton);
+		exitButton.addActionListener(new exitButton());
+		buttonPanel.add(exitButton);
 		
+		f.add(blankPanel);
+		f.add(buttonPanel);
 
 		
-		f.setLayout(new SpringLayout());
+		
 		f.setVisible(true);
 	}
 	
@@ -43,8 +60,28 @@ public class mainMenu {
 
 	}
 	
-	static class buttonClick implements ActionListener{
+	static class startButton implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			System.out.println("Start Game");
+	}	}
+	
+	static class leaderboardButton implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			System.out.println("Leaderboard");
+	}	}
+	
+	static class creditsButton implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			System.out.println("Credits");
+	}	}
+	
+	static class exitButton implements ActionListener{
 		public void actionPerformed(ActionEvent e){
 			f.dispose();
-		}	}
+	}	}
+	
+
+	
+	
+	
 }
