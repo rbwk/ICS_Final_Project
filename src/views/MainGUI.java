0e5571749210783f30624457acddd52015aa078b
Nathan C <iamnathanchu@gmail.com>
2023-12-27 20:59:57 -0500

    changes to maingui

--- src/views/MainGUI.java
+++ src/views/MainGUI.java
@@ -25,8 +25,11 @@ public class MainGUI extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private static JPanel mainScreen = new JPanel();
-	private static JPanel selectorScreen = new JPanel();
+	private static JPanel mainPanel = new JPanel();
+	private static JPanel selectorPanel = new JPanel();
+	private static JPanel leaderboardPanel = new JPanel();
+
+	
 
 	/**
 	 * Launch the application.
@@ -83,75 +86,86 @@ public class MainGUI extends JFrame {
 		layeredPane.setBackground(Color.DARK_GRAY);
 		layeredPane.setBounds(0, 0, 1920, 1080);
 		contentPane.add(layeredPane);
-		selectorScreen.setVisible(false);
+		selectorPanel.setVisible(false);
 		
 		///////////////////////////////////Main Screen///////////////////////////////////
-		
 
-		mainScreen.setBounds(0, 0, 1920, 1080);
-		layeredPane.add(mainScreen);
-		mainScreen.setLayout(null);
+		mainPanel.setBounds(0, 0, 1920, 1080);
+		layeredPane.add(mainPanel);
+		mainPanel.setLayout(null);
+		mainPanel.setVisible(true);
 		
-			JLabel lbLogo = new JLabel(new ImageIcon(MainGUI.class.getResource("/resources/logo.png")));
-			lbLogo.setBounds(275, 197, 850, 278);
-			mainScreen.add(lbLogo);
-			
-			JButton btnStart = new JButton(new ImageIcon(MainGUI.class.getResource("/resources/start_btn.png")));
-			btnStart.setContentAreaFilled(false);
-			btnStart.setBounds(410, 500, 550, 250);
-			btnStart.addActionListener(new startButton());
-			mainScreen.add(btnStart);
-			
-			JButton btnLeaderboard = new JButton(new ImageIcon(MainGUI.class.getResource("/resources/leaderboard_btn.png")));
-			btnLeaderboard.setContentAreaFilled(false);
-			btnLeaderboard.setBounds(970, 520, 100, 100);
-			//mainScreen.add(btnLeaderboard);
-			
-			JButton btnExit = new JButton(new ImageIcon(MainGUI.class.getResource("/resources/exit_btn.png")));
-			btnExit.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {dispose();}});	
-			btnExit.setContentAreaFilled(false);
-			btnExit.setBounds(1250, 20, 100, 100);
-			mainScreen.add(btnExit);
+		JLabel lbLogo = new JLabel(new ImageIcon(MainGUI.class.getResource("/resources/logo.png")));
+		lbLogo.setBounds(275, 197, 850, 278);
+		mainPanel.add(lbLogo);
+		
+		JButton btnStart = new JButton(new ImageIcon(MainGUI.class.getResource("/resources/start_btn.png")));
+		btnStart.setContentAreaFilled(false);
+		btnStart.setBounds(410, 500, 550, 250);
+		btnStart.addActionListener(new startButton());
+		mainPanel.add(btnStart);
+		
+		JButton btnLeaderboard = new JButton(new ImageIcon(MainGUI.class.getResource("/resources/leaderboard_btn.png")));
+		btnLeaderboard.setContentAreaFilled(false);
+		btnLeaderboard.setBounds(970, 520, 100, 100);
+		btnLeaderboard.addActionListener(new leaderboardButton());
+		mainPanel.add(btnLeaderboard);
+		
+		JButton btnExit = new JButton(new ImageIcon(MainGUI.class.getResource("/resources/exit_btn.png")));
+		btnExit.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {dispose();}});	
+		btnExit.setContentAreaFilled(false);
+		btnExit.setBounds(1250, 20, 100, 100);
+		mainPanel.add(btnExit);
 
-			mainScreen.setVisible(true);
 		
 		///////////////////////////////////Selector Screen///////////////////////////////////
 	
-		selectorScreen.setBounds(0, 0, 1920, 1080);
-		layeredPane.add(selectorScreen);
-		selectorScreen.setLayout(null);
-		
-		JButton btnBack = new JButton(new ImageIcon(MainGUI.class.getResource("/resources/back_btn.png")));
-		btnBack.setContentAreaFilled(false);
-		btnBack.setBounds(1250, 20, 100, 100);
-		btnBack.addActionListener(new backSelectorButton());
-		selectorScreen.add(btnBack);
+		selectorPanel.setBounds(0, 0, 1920, 1080);
+		layeredPane.add(selectorPanel);
+		selectorPanel.setLayout(null);
+		selectorPanel.setVisible(false);
+
+		JButton btnBackSelector = new JButton(new ImageIcon(MainGUI.class.getResource("/resources/back_btn.png")));
+		btnBackSelector.setContentAreaFilled(false);
+		btnBackSelector.setBounds(1250, 20, 100, 100);
+		btnBackSelector.addActionListener(new backSelectorButton());
+		selectorPanel.add(btnBackSelector);
 		
 		JButton btnPVP = new JButton("    Local Player Versus Player");
-		btnPVP.setFont(new Font("Trebuchet MS", Font.PLAIN, 22));
+		btnPVP.setFont(new Font("Trebuchet MS", Font.PLAIN, 26));
 		btnPVP.setHorizontalAlignment(SwingConstants.LEFT);
 		btnPVP.setIcon(new ImageIcon(MainGUI.class.getResource("/resources/pvp_btn.png")));
-		btnPVP.setBounds(300, 250, 500, 150);
+		btnPVP.setBounds(300, 250, 600, 150);
 		btnPVP.setContentAreaFilled(false);
-		selectorScreen.add(btnPVP);
+		selectorPanel.add(btnPVP);
 		
 		JButton btnPVC = new JButton("    Local Player Versus Computer");
-		btnPVC.setFont(new Font("Trebuchet MS", Font.PLAIN, 22));
+		btnPVC.setFont(new Font("Trebuchet MS", Font.PLAIN, 26));
 		btnPVC.setHorizontalAlignment(SwingConstants.LEFT);
 		btnPVC.setIcon(new ImageIcon(MainGUI.class.getResource("/resources/pvc_btn.png")));
-		btnPVC.setBounds(300, 500, 500, 150);
+		btnPVC.setBounds(300, 500, 600, 150);
 		btnPVC.setContentAreaFilled(false);
-		selectorScreen.add(btnPVC);
-		
-		selectorScreen.setVisible(false);
+		selectorPanel.add(btnPVC);
 		
 		///////////////////////////////////Leaderboard Screen///////////////////////////////////
 		
+		leaderboardPanel.setBounds(0, 0, 1920, 1080);
+		layeredPane.add(leaderboardPanel);
+		leaderboardPanel.setLayout(null);
+		leaderboardPanel.setVisible(false);
 		
-		///////////////////////////////////Universal///////////////////////////////////
+		JButton btnBackLeaderboard = new JButton(new ImageIcon(MainGUI.class.getResource("/resources/back_btn.png")));
+		btnBackLeaderboard.setContentAreaFilled(false);
+		btnBackLeaderboard.setBounds(1250, 20, 100, 100);
+		btnBackLeaderboard.addActionListener(new backLeaderboardButton());
+		leaderboardPanel.add(btnBackLeaderboard);
 		
-		setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{contentPane, layeredPane, btnStart, lbLogo, btnLeaderboard, mainScreen, selectorScreen, btnBack, btnPVP}));
+		
+		
+		setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{contentPane, layeredPane, btnStart, lbLogo, btnLeaderboard, mainPanel, selectorPanel, btnBackSelector, btnPVP, leaderboardPanel}));
+		
+		
 
 		
 	}
@@ -162,14 +176,26 @@ public class MainGUI extends JFrame {
 	
 	static class startButton implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			mainScreen.setVisible(false);
-			selectorScreen.setVisible(true);
+			mainPanel.setVisible(false);
+			selectorPanel.setVisible(true);
+		}
+	}
+	static class leaderboardButton implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			mainPanel.setVisible(false);
+			leaderboardPanel.setVisible(true);
 		}
 	}
 	static class backSelectorButton implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			mainScreen.setVisible(true);
-			selectorScreen.setVisible(false);
+			mainPanel.setVisible(true);
+			selectorPanel.setVisible(false);
+		}
+	}
+	static class backLeaderboardButton implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			mainPanel.setVisible(true);
+			leaderboardPanel.setVisible(false);
 		}
 	}
 
