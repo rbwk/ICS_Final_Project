ae64d3fbc84d0712600f2b315177b4beaff308b5
victoriaachi <victoria.chi26@gmail.com>
2023-12-31 11:07:10 -0500

    changed order of text file reader for data.txt

--- src/common/TextFileReader.java
+++ src/common/TextFileReader.java
@@ -1,4 +1,5 @@
 package common;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,14 +9,14 @@ import java.util.List;
 
 public class TextFileReader {
     protected String filePath;
-    
+
     protected ArrayList<String> name;
     protected ArrayList<String> gender;
     protected ArrayList<String> skin_tone;
     protected ArrayList<String> eye_color;
     protected ArrayList<String> hair_color;
     protected ArrayList<String> hair_length;
-    
+
     protected List<Boolean> showing_teeth = new ArrayList<Boolean>(Arrays.asList(true));
     protected List<Boolean> wearing_hat = new ArrayList<Boolean>(Arrays.asList(true));
     protected List<Boolean> facial_hair = new ArrayList<Boolean>(Arrays.asList(true));
@@ -23,18 +24,17 @@ public class TextFileReader {
     protected List<Boolean> glasses = new ArrayList<Boolean>(Arrays.asList(true));
 
     protected ArrayList<String> questions;
-    
 
     public TextFileReader(String filePath) {
         this.filePath = filePath;
-        
+
         this.name = new ArrayList<>();
         this.gender = new ArrayList<>();
         this.skin_tone = new ArrayList<>();
         this.eye_color = new ArrayList<>();
         this.hair_color = new ArrayList<>();
         this.hair_length = new ArrayList<>();
-        
+
         this.showing_teeth = new ArrayList<>();
         this.wearing_hat = new ArrayList<>();
         this.facial_hair = new ArrayList<>();
@@ -44,63 +44,88 @@ public class TextFileReader {
         this.questions = new ArrayList<>();
     }
 
-    public void readFile() { //Very Disgusting way but it works so don't complain
+    public void readFile() { // Very Disgusting way but it works so don't complain
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = reader.readLine()) != null) {
-            	if(filePath == "data.txt") {
-            		name.add(line);
+                if (filePath == "data.txt") {
+                    name.add(line);
                     line = reader.readLine();
                     gender.add(line);
                     line = reader.readLine();
-                    skin_tone.add(line);
-                    line = reader.readLine();
                     eye_color.add(line);
+                    line = reader.readLine();
+                    skin_tone.add(line);
                     line = reader.readLine();
                     hair_color.add(line);
                     line = reader.readLine();
-                    hair_length.add(line);
+                    facial_hair.add(Boolean.parseBoolean(line));
+                    line = reader.readLine();
+                    glasses.add(Boolean.parseBoolean(line));
                     line = reader.readLine();
                     showing_teeth.add(Boolean.parseBoolean(line));
                     line = reader.readLine();
                     wearing_hat.add(Boolean.parseBoolean(line));
                     line = reader.readLine();
-                    facial_hair.add(Boolean.parseBoolean(line));
+                    hair_length.add(line);
                     line = reader.readLine();
                     piercings.add(Boolean.parseBoolean(line));
-                    line = reader.readLine();
-                    glasses.add(Boolean.parseBoolean(line));
-            	}else if(filePath == "questions.txt"){
-            		questions.add(line);
-            	}
-                
+
+                } else if (filePath == "questions.txt") {
+                    questions.add(line);
+                }
+
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public ArrayList<String> getname() {return name;}
-    
-    public ArrayList<String> getgender() {return gender;}
-    
-    public ArrayList<String> getskin_tone() {return skin_tone;}
-    
-    public ArrayList<String> geteye_color() {return eye_color;}
-    
-    public ArrayList<String> gethair_color() {return hair_color;}
-    
-    public ArrayList<String> gethair_length() {return hair_length;}
-    
-    public List<Boolean> getshowing_teeth() {return showing_teeth;}
-    
-    public List<Boolean> getwearing_hat() {return wearing_hat;}
-    
-    public List<Boolean> getfacial_hair() {return facial_hair;}
-    
-    public List<Boolean> getpiercings() {return piercings;}
-    
-    public List<Boolean> getglasses() {return glasses;}
+    public ArrayList<String> getname() {
+        return name;
+    }
 
-    public ArrayList<String> getQuestions() {return questions;}
+    public ArrayList<String> getgender() {
+        return gender;
+    }
+
+    public ArrayList<String> getskin_tone() {
+        return skin_tone;
+    }
+
+    public ArrayList<String> geteye_color() {
+        return eye_color;
+    }
+
+    public ArrayList<String> gethair_color() {
+        return hair_color;
+    }
+
+    public ArrayList<String> gethair_length() {
+        return hair_length;
+    }
+
+    public List<Boolean> getshowing_teeth() {
+        return showing_teeth;
+    }
+
+    public List<Boolean> getwearing_hat() {
+        return wearing_hat;
+    }
+
+    public List<Boolean> getfacial_hair() {
+        return facial_hair;
+    }
+
+    public List<Boolean> getpiercings() {
+        return piercings;
+    }
+
+    public List<Boolean> getglasses() {
+        return glasses;
+    }
+
+    public ArrayList<String> getQuestions() {
+        return questions;
+    }
 }
--- src/views/MainGUI.java
+++ src/views/MainGUI.java
@@ -29,8 +29,6 @@ public class MainGUI extends JFrame {
 	private static JPanel selectorPanel = new JPanel();
 	private static JPanel leaderboardPanel = new JPanel();
 
-	
-
 	/**
 	 * Launch the application.
 	 */
@@ -54,26 +52,28 @@ public class MainGUI extends JFrame {
 
 	/**
 	 * Create the frame.
-	 * @throws IOException 
+	 * 
+	 * @throws IOException
 	 */
 	public MainGUI() throws IOException {
-		
-		
+
 		initComponents();
 		createEvents();
-		
 
 	}
-	
+
 	private void initComponents() throws IOException {
-		
+
 		setBackground(Color.WHITE);
-		setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
 		setUndecorated(true);
-		
-		setIconImage(Toolkit.getDefaultToolkit().getImage(MainGUI.class.getResource("/resources/icon_logo.png"))); //Create the icon image 
+
+		setIconImage(Toolkit.getDefaultToolkit().getImage(MainGUI.class.getResource("/resources/icon_logo.png"))); // Create
+																													// the
+																													// icon
+																													// image
 		setTitle("Exiting Guess Who Game !!!");
-		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(0, 0, 1920, 1080);
 		contentPane = new JPanel();
@@ -81,46 +81,51 @@ public class MainGUI extends JFrame {
 		contentPane.setBorder(null);
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLayeredPane layeredPane = new JLayeredPane();
 		layeredPane.setBackground(Color.DARK_GRAY);
 		layeredPane.setBounds(0, 0, 1920, 1080);
 		contentPane.add(layeredPane);
 		selectorPanel.setVisible(false);
-		
-		///////////////////////////////////Main Screen///////////////////////////////////
+
+		/////////////////////////////////// Main
+		/////////////////////////////////// Screen///////////////////////////////////
 
 		mainPanel.setBounds(0, 0, 1920, 1080);
 		layeredPane.add(mainPanel);
 		mainPanel.setLayout(null);
 		mainPanel.setVisible(true);
-		
+
 		JLabel lbLogo = new JLabel(new ImageIcon(MainGUI.class.getResource("/resources/logo.png")));
 		lbLogo.setBounds(275, 197, 850, 278);
 		mainPanel.add(lbLogo);
-		
+
 		JButton btnStart = new JButton(new ImageIcon(MainGUI.class.getResource("/resources/start_btn.png")));
 		btnStart.setContentAreaFilled(false);
 		btnStart.setBounds(410, 500, 550, 250);
 		btnStart.addActionListener(new startButton());
 		mainPanel.add(btnStart);
-		
-		JButton btnLeaderboard = new JButton(new ImageIcon(MainGUI.class.getResource("/resources/leaderboard_btn.png")));
+
+		JButton btnLeaderboard = new JButton(
+				new ImageIcon(MainGUI.class.getResource("/resources/leaderboard_btn.png")));
 		btnLeaderboard.setContentAreaFilled(false);
-		btnLeaderboard.setBounds(970, 520, 100, 100);
+		btnLeaderboard.setBounds(600, 725, 100, 100);
 		btnLeaderboard.addActionListener(new leaderboardButton());
 		mainPanel.add(btnLeaderboard);
-		
+
 		JButton btnExit = new JButton(new ImageIcon(MainGUI.class.getResource("/resources/exit_btn.png")));
 		btnExit.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {dispose();}});	
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
 		btnExit.setContentAreaFilled(false);
 		btnExit.setBounds(1250, 20, 100, 100);
 		mainPanel.add(btnExit);
 
-		
-		///////////////////////////////////Selector Screen///////////////////////////////////
-	
+		/////////////////////////////////// Selector
+		/////////////////////////////////// Screen///////////////////////////////////
+
 		selectorPanel.setBounds(0, 0, 1920, 1080);
 		layeredPane.add(selectorPanel);
 		selectorPanel.setLayout(null);
@@ -131,7 +136,7 @@ public class MainGUI extends JFrame {
 		btnBackSelector.setBounds(1250, 20, 100, 100);
 		btnBackSelector.addActionListener(new backSelectorButton());
 		selectorPanel.add(btnBackSelector);
-		
+
 		JButton btnPVP = new JButton("    Local Player Versus Player");
 		btnPVP.setFont(new Font("Trebuchet MS", Font.PLAIN, 26));
 		btnPVP.setHorizontalAlignment(SwingConstants.LEFT);
@@ -139,7 +144,7 @@ public class MainGUI extends JFrame {
 		btnPVP.setBounds(300, 250, 600, 150);
 		btnPVP.setContentAreaFilled(false);
 		selectorPanel.add(btnPVP);
-		
+
 		JButton btnPVC = new JButton("    Local Player Versus Computer");
 		btnPVC.setFont(new Font("Trebuchet MS", Font.PLAIN, 26));
 		btnPVC.setHorizontalAlignment(SwingConstants.LEFT);
@@ -147,57 +152,56 @@ public class MainGUI extends JFrame {
 		btnPVC.setBounds(300, 500, 600, 150);
 		btnPVC.setContentAreaFilled(false);
 		selectorPanel.add(btnPVC);
-		
-		///////////////////////////////////Leaderboard Screen///////////////////////////////////
-		
+
+		/////////////////////////////////// Leaderboard
+		/////////////////////////////////// Screen///////////////////////////////////
+
 		leaderboardPanel.setBounds(0, 0, 1920, 1080);
 		layeredPane.add(leaderboardPanel);
 		leaderboardPanel.setLayout(null);
 		leaderboardPanel.setVisible(false);
-		
+
 		JButton btnBackLeaderboard = new JButton(new ImageIcon(MainGUI.class.getResource("/resources/back_btn.png")));
 		btnBackLeaderboard.setContentAreaFilled(false);
 		btnBackLeaderboard.setBounds(1250, 20, 100, 100);
 		btnBackLeaderboard.addActionListener(new backLeaderboardButton());
 		leaderboardPanel.add(btnBackLeaderboard);
-		
-		
-		
-		setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{contentPane, layeredPane, btnStart, lbLogo, btnLeaderboard, mainPanel, selectorPanel, btnBackSelector, btnPVP, leaderboardPanel}));
-		
-		
 
-		
+		setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[] { contentPane, layeredPane, btnStart, lbLogo,
+				btnLeaderboard, mainPanel, selectorPanel, btnBackSelector, btnPVP, leaderboardPanel }));
+
 	}
-	
+
 	private void createEvents() {
-		
+
 	}
-	
+
 	static class startButton implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			mainPanel.setVisible(false);
 			selectorPanel.setVisible(true);
 		}
 	}
+
 	static class leaderboardButton implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			mainPanel.setVisible(false);
 			leaderboardPanel.setVisible(true);
 		}
 	}
+
 	static class backSelectorButton implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			mainPanel.setVisible(true);
 			selectorPanel.setVisible(false);
 		}
 	}
+
 	static class backLeaderboardButton implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			mainPanel.setVisible(true);
 			leaderboardPanel.setVisible(false);
 		}
 	}
-
 
 }
