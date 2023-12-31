6dc63f99d7b8f31df360822a9bf0006db090846c
aryanispotato <144962521+aryanispotato@users.noreply.github.com>
2023-12-31 13:56:19 -0500

    Update game.java
    Began building logic for this file, still having issues with importing the character class, i am going to make a clone of the class specifically to be imported into this class.

--- src/common/game.java
+++ src/common/game.java
@@ -1,24 +1,47 @@
 package common;
 
 import java.util.ArrayList;
+import common.TextFileReader; // Import TextFileReaders
+import javax.xml.stream.events.Characters;
 
 public class game {
+	protected static int diff; // Difficulty level
+    protected static ArrayList<String> questions; // List of questions
+    protected static ArrayList<Characters> characters; // List of characters
+	protected static ArrayList<Characters> p1_characters; // Player 1 list of remaining characters
+    protected static ArrayList<Characters> p2_characters; // Player 2 list of remaining characters
+	
+    public static void main(String[] args) {
+        characters = new ArrayList<>();
+        questions = new ArrayList<>();
 
-	//Variables [P1 = Player 1, P2 = Player 2]
-	protected static int diff;		//Difficulty
-	protected static int p1_num;	//Player 1 Amount of Characters Left
-	protected static int p2_num;	//Player 2 Amount of Characters Left
-	protected static ArrayList<String> Questions;
+        // Initialize TextFileReader for characters
+        TextFileReader characterReader = new TextFileReader("data.txt");
+        characterReader.readFile();
 
+        // Create Characters objects and add them to the characters list
+        for (int i = 0; i < characterReader.getname().size(); i++) {
+            Characters character = new Characters(
+                characterReader.getname().get(i),
+                characterReader.getgender().get(i),
+                characterReader.getskin_tone().get(i),
+                characterReader.geteye_color().get(i),
+                characterReader.gethair_color().get(i),
+                characterReader.gethair_length().get(i),
+                characterReader.getshowing_teeth().get(i),
+                characterReader.getwearing_hat().get(i),
+                characterReader.getfacial_hair().get(i),
+                characterReader.getpiercings().get(i),
+                characterReader.getglasses().get(i)
+            );
+            characters.add(character);
+        }
 
-	public static void main(String[] args) {
-		TextFileReader C = new TextFileReader("data.txt"); //Characters
-		C.readFile();
-		System.out.println(C.getname());
-		
-		TextFileReader Q = new TextFileReader("questions.txt"); //Questions
-		Q.readFile();
-		System.out.println(Q.getQuestions());
-	}
+        // Initialize TextFileReader for questions
+        TextFileReader questionReader = new TextFileReader("questions.txt");
+        questionReader.readFile();
+        questions = questionReader.getQuestions();
+
+    }
 
 }
