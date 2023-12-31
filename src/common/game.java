6454900942a0f5784d97b6a23b4d4eb77dd654b2
aryanispotato <144962521+aryanispotato@users.noreply.github.com>
2023-12-31 14:19:28 -0500

    Update game.java
    Finished correcting errors in the file

--- src/common/game.java
+++ src/common/game.java
@@ -1,15 +1,13 @@
 package common;
 
 import java.util.ArrayList;
-import common.TextFileReader; // Import TextFileReaders
-import javax.xml.stream.events.Characters;
 
 public class game {
 	protected static int diff; // Difficulty level
     protected static ArrayList<String> questions; // List of questions
-    protected static ArrayList<Characters> characters; // List of characters
-	protected static ArrayList<Characters> p1_characters; // Player 1 list of remaining characters
-    protected static ArrayList<Characters> p2_characters; // Player 2 list of remaining characters
+    protected static ArrayList<GameCharacter> characters; // List of characters
+    protected static ArrayList<GameCharacter> p1_characters; // Player 1 list of remaining characters
+    protected static ArrayList<GameCharacter> p2_characters; // Player 2 list of remaining characters
 	
     public static void main(String[] args) {
         characters = new ArrayList<>();
@@ -19,21 +17,20 @@ public class game {
         TextFileReader characterReader = new TextFileReader("data.txt");
         characterReader.readFile();
 
-        // Create Characters objects and add them to the characters list
+        // Create GameCharacter objects and add them to the characters list
         for (int i = 0; i < characterReader.getname().size(); i++) {
-            Characters character = new Characters(
-                characterReader.getname().get(i),
-                characterReader.getgender().get(i),
-                characterReader.getskin_tone().get(i),
-                characterReader.geteye_color().get(i),
-                characterReader.gethair_color().get(i),
-                characterReader.gethair_length().get(i),
-                characterReader.getshowing_teeth().get(i),
-                characterReader.getwearing_hat().get(i),
-                characterReader.getfacial_hair().get(i),
-                characterReader.getpiercings().get(i),
-                characterReader.getglasses().get(i)
-            );
+            GameCharacter character = new GameCharacter();
+            character.setName(characterReader.getname().get(i));
+            character.setGender(characterReader.getgender().get(i));
+            character.setSkinTone(characterReader.getskin_tone().get(i));
+            character.setEyeColour(characterReader.geteye_color().get(i));
+            character.setHairColour(characterReader.gethair_color().get(i));
+            character.setHairLength(characterReader.gethair_length().get(i));
+            character.setShowingTeeth(characterReader.getshowing_teeth().get(i));
+            character.setWearingHat(characterReader.getwearing_hat().get(i));
+            character.setFacialHair(characterReader.getfacial_hair().get(i));
+            character.setPiercings(characterReader.getpiercings().get(i));
+            character.setGlasses(characterReader.getglasses().get(i));
             characters.add(character);
         }
 
